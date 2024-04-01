66a961607219159a0722345107006f2c0e537310
priyanshuhaldar007 <priyanshu1843@gmail.com>
2024-04-01 18:01:35 +0530

    added menu for easy navigation

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './styles/globalstyles.scss';
 import { PageA } from './pages/PageA';
 import { PageB } from './pages/PageB';
 import { HomePage } from './pages/HomePage';
+import { Menu } from './Component/Menu';
 
 function App() {
   return (
     <div className="App">
+      <Menu/>
       <Router>
         <Routes>
           <Route path='/pageA' element={<PageA/>}/>
---
+++ src/Component/Menu.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Menu = () => {
+  return (
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/pagea">Page A</a></li>
+        <li><a href="/pageb">Page B</a></li>
+    </ul>
+  )
+}
